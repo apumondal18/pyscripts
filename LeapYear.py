0cf467f39e5c207df858492ed4425b49e2d3aff3
apumondal18 <apumondal18@users.noreply.github.com>
2019-03-07 00:46:10 +0530

    Check Leap Year

---
+++ LeapYear.py
@@ -0,0 +1,18 @@
+#Input year as integer to check if it's leap year or not
+def is_leap(year):
+    leap = False
+    if year % 100 == 0:
+        if year % 400 == 0:
+            leap = True
+        else:
+            leap = False
+    elif year % 4 == 0:
+        leap = True
+
+    # Write your logic here
+
+    return leap
+
+
+year = int(input())
+print(is_leap(year))
