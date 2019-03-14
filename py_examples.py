2ac9e147427a6c30fbe8f2c6e624eaedfe6afb48
apumondal18 <apumondal18@users.noreply.github.com>
2019-03-15 02:14:41 +0530

    Python Examples Combined

---
+++ py_examples.py
@@ -0,0 +1,38 @@
+print("My Name is Apu")
+print("I said don't call me Appu")
+print('Please call me "Apu"')
+print(" Check the order ", 25 + 5 - 6 + 8 / 2 )
+print(" Check the order again ", 25 + 5 - ( 6 + 8 ) / 2 )
+
+age = 31
+print("My age is", age, "years")
+print(f"My age is {age} years")
+i_said = f"My age is {age} years"
+
+print('*' * 10)
+
+a = 'B'
+b = "r"
+c = 'e'
+d = "a"
+e = 'k'
+
+print(a+b,end=' ')
+print(c+d+e)
+
+print(a+b)
+print(c+d+e)
+
+abc = "{} {}"
+print(abc.format(1,2,3,4))
+print(abc.format(abc,abc))
+
+print("Apu Mondal")
+print("Apu \nMondal")
+print('''hello
+everyone''')
+
+'''this is
+a comment here not inside print'''
+
+print("I'm Hungry for \"Pizza\"")
