3afae034b258a541c50f97879e358027f2d29c9e
apumondal18 <apumondal18@users.noreply.github.com>
2019-03-16 01:00:02 +0530

    Python Examples 3 Combined

---
+++ py_examples_3.py
@@ -0,0 +1,18 @@
+def names(*babies):
+    a, b = babies
+    print(f"Babies are {a} and {b}")
+
+def diff_babies(baby1, baby2):
+    print(f"Babies are {baby1} and {baby2}")
+
+def cute_baby(baby):
+    print(f"My {baby} is cute")
+
+def nobaby():
+    print("I don't have a baby")
+
+#lets call the babies
+names('Ana','Rock')
+diff_babies('Shyam','Prem')
+cute_baby('Adrija')
+nobaby()
