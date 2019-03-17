a4e299375bab75e18b778ff3fe389bc901882f6d
apumondal18 <apumondal18@users.noreply.github.com>
2019-03-18 03:29:20 +0530

    Referring Examples

---
+++ py_examples_4.py
@@ -0,0 +1,13 @@
+def split_word(word):
+    return word.split(' ')
+
+def sorter(word):
+    return sorted(word)
+
+def first_word(word):
+    wd = word.pop(0)
+    return(wd)
+
+def last_word(word):
+    wd = word.pop(-1)
+    return(wd)
---
+++ py_examples_4_1.py
@@ -0,0 +1,8 @@
+import py_examples_4
+
+word = 'Brown Fox and White Rabbit'
+
+print(py_examples_4.split_word(word))
+print(py_examples_4.sorter(word))
+print(py_examples_4.first_word(py_examples_4.split_word(word)))
+print(py_examples_4.last_word(py_examples_4.split_word(word)))
